5ee6830efe97ba789b8f1aef337523f26473487f
spriveen <rpeanuka@gmail.com>
2024-09-01 01:30:06 -0700

    update footer. tsx

--- my-app/components/Footer.tsx
+++ my-app/components/Footer.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import { footerLinks } from '@/constants'
+import Link from 'next/link'
+import Image from 'next/image'
+
+type ColumnProps = {
+    title: string
+    links: Array<string>; // Lowercase 'links'
+}
+
+const FooterColumn = ({ title, links }: ColumnProps) => (
+    <div className='footer_column'>
+        <h4 className='font-semibold'>{title}</h4>
+        <ul className='flex flex-col gap-2 font-normal'>
+            {links.map((link, index) => (
+                <Link href="/" key={index}>
+                    {link}
+                </Link>
+            ))}
+        </ul>
+    </div>
+)
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+    return (
+        <footer className='flexStart footer'>
+            <div className='flex flex-col gap-12 w-full'>
+                <div className='flex items-start flex-col'>
+                    <Image
+                        src="/logo-purple.svg"
+                        width={115}
+                        height={38}
+                        alt='Flexibble'
+                    />
+                    <p className='text-start text-sm font-normal mt-5 max-w-xs'>
+                        Flexibble is the world's leading community for creatives to share, grow, and get hired.
+                    </p>
+                </div>
+                <div className='flex flex-wrap gap-12'>
+                    <FooterColumn title={footerLinks[0].title} links={footerLinks[0].links} />
+                </div>
+            </div>
+        </footer>
+    )
 }
 
 export default Footer
--- my-app/components/Navbar.tsx
+++ my-app/components/Navbar.tsx
@@ -1,8 +1,11 @@
+import { NavLinks } from '@/constants'
 import Image from 'next/image'
 import Link from 'next/link'
+import AuthProviders from './AuthProviders';
 
 
 const Navbar = () => {
+    const session = null;
   return (
     <nav className='flexBetween navbar'>
         <div className='flex-1 flexStart gap-10'>
@@ -16,8 +19,26 @@ const Navbar = () => {
             </Link>
             <ul className='xl:flex hidden
             text-small gap-7'>
+            {NavLinks.map((link) => (
+              <Link href={link.href} key={link.key}>
+                {link.text}
+              </Link>
+            ))}
 
             </ul>
+</div>
+<div className='flex-center gap-4'>
+    {session ? (
+      <>
+      UserPhoto
+      <Link href="/create-project">
+      Share Work
+      </Link>
+      </>
+    ):(
+      <AuthProviders/>
+    )}
+    
 </div>
 </nav> 
   )
